dc9a131c12971f06fe979f2daf061fe5659be9c1
Carlos Leon <ctr486@yahoo.com>
2023-05-18 19:19:44 -0500

    adding the view for the csv file with the matplotlib graph

--- AInterface/AInterface/settings.py
+++ AInterface/AInterface/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,7 +66,7 @@ ROOT_URLCONF = 'AInterface.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'temp')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,8 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-import os
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- AInterface/Apps/KNeighborsClassifier/views.py
+++ AInterface/Apps/KNeighborsClassifier/views.py
@@ -3,6 +3,10 @@ from .forms import CSVDataForm
 from .models import CSVFile
 from django.views import View
 from django.urls import reverse_lazy
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import matplotlib.backends.backend_agg as agg
 
 
 class CsvCreateView(View):
@@ -15,22 +19,53 @@ class CsvCreateView(View):
 
     def post(self, request, pk=None):
         form = CSVDataForm(request.POST, request.FILES or None)
+        success_url = 'KNeighborsClassifier:csv_detail'
 
         if not form.is_valid():
             ctx = {'form': form}
             return render(request, self.template_name, ctx)
 
         csv_file = form.save()
-        # pic.owner = self.request.user
-        # csv_file.save()
-        # form.save_m2m()
-        return redirect('KNeighborsClassifier:csv_detail', pk=csv_file.pk)
+        return redirect(success_url, pk=csv_file.pk)
     
 
 class CsvDetailView(View):
     model = CSVFile
     template_name = "KNeighborsClassifier/csv_detail.html"
-    def get(self, request, pk) :
-        x = CSVFile.objects.get(id=pk)
-        context = { 'csv_file' : x, }
+
+
+class CsvDetailView(View):
+    model = CSVFile
+    template_name = "KNeighborsClassifier/csv_detail.html"
+
+    def get(self, request, pk):
+        csv_file = CSVFile.objects.get(id=pk)
+
+        # Read the CSV data and create a plot
+        data = read_csv_data(csv_file.file.path)
+        x = data['x']
+        y = data['y']
+
+        # Plot creation
+        fig, ax = plt.subplots()
+        ax.plot(x, y)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
+        ax.set_title('Matplotlib Plot')
+
+        # Render the plot to a canvas
+        canvas = agg.FigureCanvasAgg(fig)
+        canvas.draw()
+
+        # Save the plot to a temporary file within the media directory
+        temp_file = os.path.join('media', 'plot.png')
+        canvas.print_png(temp_file)
+        # Pass the temporary file path to the template
+        context = {'csv_file': csv_file, 'plot_file': temp_file}
         return render(request, self.template_name, context)
+
+
+def read_csv_data(file_path):
+
+    data = pd.read_csv(file_path)
+    return {'x': data['radius_mean'], 'y': data['texture_mean']}
